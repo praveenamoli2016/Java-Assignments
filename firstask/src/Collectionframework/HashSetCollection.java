e42ce05363a8079875d1f5448113a9fd2c7ad3db
Praveen Tiwari <pkumartiwari56@gmail.com>
2022-03-17 17:15:17 +0530

    fixed review and comments

--- firstask/src/Collectionframework/HashSetCollection.java
+++ firstask/src/Collectionframework/HashSetCollection.java
@@ -2,6 +2,7 @@ package Collectionframework;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class HashSetCollection {
 	public static void main(String[] args) {
@@ -9,6 +10,11 @@ public class HashSetCollection {
 		h.add("praveen");
 		h.add(10);
 		h.add("c");
+		System.out.println(h);
+		List l=new ArrayList();
+		l.add("praveen");
+		l.add(10);
+		l.add("c");
 		System.out.println(h);
 		
 	}
---
+++ firstask/src/Collectionframework/Map.java
@@ -0,0 +1,15 @@
+package Collectionframework;
+
+import java.util.HashMap;
+
+public class Map  {
+		public static void main(String[] args) {
+			HashMap hm=new HashMap();
+			hm.put(101,"praveen");
+			hm.put(102,"rahul");
+			hm.put(103,"rishab");
+			System.out.println(hm);
+			
+		}
+	}
+
